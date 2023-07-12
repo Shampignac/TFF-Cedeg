3e142f5937cb0b27310abbb2c4f55eccdffadf36
unknown <shampignac@gmil.com>
2023-07-12 17:21:53 +0200

    thirdCommit

--- pages/conseils.vue
+++ pages/conseils.vue
@@ -4,7 +4,7 @@
       <div
         class="absolute top-0 left-0 right-0 bottom-0 z-0"
         style="
-          background-image: url('../public/medias/Green-Life.png');
+          background-image: url('public/medias/Green-Life.png');
           background-size: cover;
           background-position: center;
           opacity: 0.4;
