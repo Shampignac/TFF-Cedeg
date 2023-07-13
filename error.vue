caa62f29a30c374f6cef9ccc86f93a9c7c301479
unknown <shampignac@gmil.com>
2023-07-13 21:23:21 +0200

    error

--- error.vue
+++ error.vue
@@ -1,7 +1,30 @@
 <template>
-
-
-
-
-    
+  <div
+    class="flex flex-col items-center justify-center h-screen bg-black text-white"
+  >
+    <h1 class="text-6xl font-bold mb-4">Oups! Erreur 404</h1>
+    <p class="text-xl mt-8">
+      C'est un peu gênant... On dirait que cette page s'est perdue dans
+      l'espace.
+    </p>
+    <div class="gif-container">
+      <img
+        src="public\medias\IMG_4147.gif"
+        alt="GIF Error 404"
+        class="w-80 h-auto"
+      />
+    </div>
+  </div>
 </template>
+
+<style>
+.gif-container {
+  margin-top: 2rem;
+}
+
+@media (min-width: 768px) {
+  .gif-container {
+    max-width: 400px;
+  }
+}
+</style>
